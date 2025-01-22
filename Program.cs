8044ab72e32e214f48837c5673f2d61f3595196a
alainprof <amartel@cstj.qc.ca>
2025-01-22 09:28:07 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AtelierOO_101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Atelier de dev de proj OO pour le groupe 2C6-101!");
+
+            AfficherMenu();
+        }
+
+        static void AfficherMenu()
+        {
+            Console.WriteLine("F: outils Financiers ");
+            Console.WriteLine("H: Humanité");
+            Console.WriteLine("Q: Quitter");
+        }
+    }
+}
